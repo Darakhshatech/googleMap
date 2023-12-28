b928acfd74f746b371e6f2345f44ff924f112f75
Darakhshatech <154502937+Darakhshatech@users.noreply.github.com>
2023-12-28 09:01:32 +0000

    done task

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
     { lat: 37.7749, lng: -122.4194 }, // Example coordinate 1 (San Francisco, CA)
     { lat: 34.0522, lng: -118.2437 }, // Example coordinate 2 (Los Angeles, CA)
     { lat: 41.8781, lng: -87.6298 },  // Example coordinate 3 (Chicago, IL)
-    { lat: 41.2781, lng: -87.6298 },  // Example coordinate 3 (Chicago, IL)
-    { lat: 41.1781, lng: -87.6298 },  // Example coordinate 3 (Chicago, IL)
   ];
 
   const centerCoordinate = listCoordinates.length > 0 ? listCoordinates[0] : null; // Use the first coordinate as the center
