3e2ca559460147c35988cab7aa0d82a80bd0da43
Darakhshatech <154502937+Darakhshatech@users.noreply.github.com>
2023-12-28 08:57:08 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { useState } from "react";
-import "./App.css";
-import SearchLocationInput from "./components/GooglePlcasesApi";
+import React from "react";
 import MapComponent from "./components/Map";
 
 function App() {
-  const [selectedLocation, setSelectedLocation] = useState({
-    lat: 28.7041,
-    lng: 77.1025,
-  });
+  const listCoordinates = [
+    { lat: 37.7749, lng: -122.4194 }, // Example coordinate 1 (San Francisco, CA)
+    { lat: 34.0522, lng: -118.2437 }, // Example coordinate 2 (Los Angeles, CA)
+    { lat: 41.8781, lng: -87.6298 },  // Example coordinate 3 (Chicago, IL)
+    { lat: 41.2781, lng: -87.6298 },  // Example coordinate 3 (Chicago, IL)
+    { lat: 41.1781, lng: -87.6298 },  // Example coordinate 3 (Chicago, IL)
+  ];
+
+  const centerCoordinate = listCoordinates.length > 0 ? listCoordinates[0] : null; // Use the first coordinate as the center
+
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      {/* <SearchLocationInput setSelectedLocation={setSelectedLocation} /> */}
-      <MapComponent selectedLocation={selectedLocation} />
+      <MapComponent coordinatesList={listCoordinates} centerCoordinate={centerCoordinate} />
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,35 +1,173 @@
-import React from "react";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
+import { GoogleMap, useLoadScript, Marker, InfoWindow, MarkerF , Polyline} from "@react-google-maps/api";
+
+
 import { REACT_APP_GOOGLE_MAPS_KEY } from "../constants/constants";
-const MapComponent = ({ selectedLocation }) => {
-  console.log(selectedLocation);
+const MapComponent = ({ coordinatesList, centerCoordinate }) => {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: REACT_APP_GOOGLE_MAPS_KEY,
   });
-  const mapRef = React.useRef();
-  const onMapLoad = React.useCallback((map) => {
-    mapRef.current = map;
-  }, []);
-  if (loadError) return "Error";
-  if (!isLoaded) return "Maps";
+  const polylineOptions = {
+    strokeColor: "#0000FF",
+    strokeOpacity: 0.7,
+    strokeWeight: 4,
+  };
+  
+  const [polylines, setPolylines] = useState([]);
+
+
+  const [directions, setDirections] = useState(null);
+
+
+  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [distance, setDistance] = useState(null);
+  const calculateDirections = () => {
+    if (isLoaded && centerCoordinate && coordinatesList && coordinatesList.length > 0) {
+      const directionsService = new window.google.maps.DirectionsService();
+      const newPolylines = [];
+  
+      coordinatesList.forEach((destination) => {
+        directionsService.route(
+          {
+            origin: centerCoordinate,
+            destination: destination,
+            travelMode: "DRIVING",
+          },
+          (response, status) => {
+            if (status === "OK") {
+              const polyline = new window.google.maps.Polyline({
+                path: response.routes[0].overview_path,
+                strokeColor: "#0000FF",
+                strokeOpacity: 0.7,
+                strokeWeight: 4,
+              });
+  
+              newPolylines.push(polyline);
+              setPolylines([...newPolylines]); // Update the state
+            } else {
+              console.error("Error calculating directions:", status);
+            }
+          }
+        );
+      });
+    }
+  };
+  
+
+  
+  useEffect(() => {
+    console.log(coordinatesList , centerCoordinate);
+    calculateDirections();
+    if (isLoaded && centerCoordinate && coordinatesList && coordinatesList.length > 0) {
+      const distanceMatrixService = new window.google.maps.DistanceMatrixService();
+      distanceMatrixService.getDistanceMatrix(
+        {
+          origins: [centerCoordinate],
+          destinations: coordinatesList,
+          travelMode: "DRIVING",
+        },
+        (response, status) => {
+          if (status === "OK") {
+            // console.log(response)
+            const meters = response.rows[0].elements.map((element) => element.distance.value);
+
+            const kilometers = meters.map((meter) => (meter / 1000).toFixed(2));
+            setDistance(kilometers);
+            console.log(kilometers)
+          }
+        }
+      );
+    }
+  }, [isLoaded, centerCoordinate, coordinatesList]);
+
+  const mapContainerStyle = {
+    height: "800px",
+  };
+
+  const center = centerCoordinate || coordinatesList[0];
+
+  const onMapLoad = (map) => {
+    // Fit the map bounds to include all markers and directions
+    const bounds = new window.google.maps.LatLngBounds();
+  
+    // Extend bounds for markers
+    coordinatesList.forEach((coordinate) => {
+      bounds.extend(new window.google.maps.LatLng(coordinate.lat, coordinate.lng));
+    });
+  
+    // Extend bounds for directions
+    if (directions) {
+      const overviewPath = directions.routes[0].overview_path;
+      overviewPath.forEach((path) => {
+        bounds.extend(path);
+      });
+    }
+  
+    map.fitBounds(bounds);
+  };
+  
+  
+
+  const handleMarkerClick = (marker) => {
+    setSelectedMarker(marker);
+  };
+
+
+
+  const renderMarkers = () => {
+    console.log("Rendering markers:", coordinatesList);
+    return coordinatesList.map((coordinate, index) => (
+      <MarkerF
+        key={index}
+        position={coordinate}
+        onClick={() => handleMarkerClick(coordinate)}
+        icon={{
+          url: "https://maps.google.com/mapfiles/ms/icons/green-dot.png",
+          scaledSize: new window.google.maps.Size(30, 30),
+        }}
+        zIndex={1000} // Adjust the z-index as needed
+        
+      />
+    ));
+  };
+  
+  
+  
+  
+
+  if (loadError) {
+    console.error("Map cannot be loaded:", loadError);
+    return "Error loading map";
+  }
+
+  if (!isLoaded) return <div>Loading...</div>;
 
   return (
     <div style={{ marginTop: "50px" }}>
-      <GoogleMap
-        mapContainerStyle={{
-          height: "800px",
-        }}
-        center={selectedLocation}
-        zoom={13}
-        onLoad={onMapLoad}
-      >
-        <MarkerF
-          position={selectedLocation}
-          icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
-        />
-      </GoogleMap>
+<GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={13} onLoad={onMapLoad}>
+  {renderMarkers()}
+
+  {polylines.map((polyline, index) => (
+    <Polyline key={index} path={polyline.getPath().getArray()} options={polylineOptions} />
+  ))}
+
+  {selectedMarker && (
+    <InfoWindow
+      position={selectedMarker}
+      onCloseClick={() => setSelectedMarker(null)}
+    >
+      <div>
+        <p>{`Distance from center: ${distance && distance[coordinatesList.indexOf(selectedMarker)]} km`}</p>
+      </div>
+    </InfoWindow>
+  )}
+</GoogleMap>
+
+
+
     </div>
   );
 };
 
 export default MapComponent;
+
